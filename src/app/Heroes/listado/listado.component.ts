2635b441c13f02603795db104bd69658cc136324
Farreset <ericfarrejove@gmail.com>
2021-10-18 20:09:36 +0200

    A
    Q

--- src/app/Heroes/listado/listado.component.ts
+++ src/app/Heroes/listado/listado.component.ts
@@ -8,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class ListadoComponent{
 
   heroes : string[]=['Daniwoman','Electroiman','Musulman','Guzman'];
+  eliminar : string = "";
+  clickado : string ="";
 
+  borrarHeroe(){
+   if(this.heroes.length == 0){
+    this.eliminar= "";
+   } else{
+    this.eliminar = this.heroes[0];
+   }
+   this.heroes.splice(0,1);
+  }
+  clickar(heroeclickado : string){
+    this.clickado = heroeclickado
+   }
+   
 
 }
